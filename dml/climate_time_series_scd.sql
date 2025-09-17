b4ec32d6cc63a495b4d8e5e3cc8b91f3da26353d
Airi T Oye <16245878+airitoye@users.noreply.github.com>
2025-09-17 11:55:04 -0700

    Adding hyperlink to data

--- dml/climate_time_series_scd.sql
+++ dml/climate_time_series_scd.sql
@@ -1,6 +1,7 @@
 /*
 -- Title: Daily Climate Time Series transformed to SCD (dml)
 -- Purpose: Build a scd table to optimize performance of a daily climate time series dataset for downstream BI tools
+-- Data link: https://www.kaggle.com/datasets/sumanthvrao/daily-climate-time-series-data?resource=download
 -- Author: Airi Oye
 -- Creation Date: 9/17/25
 */
